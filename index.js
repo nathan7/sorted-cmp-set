d9e5eb139adbad1e384003bb1806d752d236bfa3
Nathan Zadoks <nathan@nathan7.eu>
2014-07-13 04:13:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+var search = require('binary-search')
+
+function SortedArray(cmp, arr) {
+  if (typeof cmp != 'function')
+    throw new TypeError('comparator must be a function')
+
+  this.arr = arr || []
+  this.cmp = cmp
+}
+
+SortedArray.prototype.insert = function(element) {
+  var index = search(this.arr, element, this.cmp)
+  if (index >= 0) {
+    this.arr[index] = element
+    return false
+  }
+
+  this.arr.splice(~index, 0, element)
+  return true
+}
+
+SortedArray.prototype.indexOf = function(element) {
+  var index = search(this.arr, element, this.cmp)
+  return index >= 0
+    ? index
+    : -1
+}
+
+SortedArray.prototype.remove = function(element) {
+  var index = search(this.arr, element, this.cmp)
+  if (index < 0)
+    return false
+
+  this.arr.splice(index, 1)
+  return true
+}
